b7d2342865605f14f2a0ec39660eac51f2d42d23
n00846095 <cici.carter@gmail.com>
2016-11-08 00:45:22 -0500

    Added Manage Volunteers form
    Added a new form.

---
+++ ECS/ManageVolunteers.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ECS.BLL;
+using ECS.Model;
+
+namespace ECS
+{
+    public partial class ManageVolunteers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void RadGrid1_NeedDataSource(object source, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            Bll bll = new Bll();
+            RadGrid1.DataSource = bll.GetVolunteers();
+        }
+    }
+}
