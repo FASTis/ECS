bd26b545bc44d2032684b41550f782437485b8ea
n00846095 <cici.carter@gmail.com>
2017-01-24 13:38:25 -0500

    Added GetPIN() method.

--- ECS/ForgottenPassword.aspx
+++ ECS/ForgottenPassword.aspx
@@ -26,11 +26,9 @@
                 <!-- submit/cancel button -->
                 <br />
                 <div class="form-group">
-                    <div class="row">
-                        <div class="col-lg-10 col-lg-offset-2">
+                    <div class="col-lg-10 col-lg-offset-2">
                             <asp:Button ID="btnSubmit" runat="server" CssClass="btn btn-primary" Text="Submit" OnClick="btnSubmit_Click" />
                             <asp:Button ID="btnCancel" runat="server" CssClass="btn btn-warning" Text="Cancel" OnClick="btnCancel_Click" />
-                        </div>
                     </div>
                 </div>
             </fieldset>
--- ECS/ForgottenPassword.aspx.cs
+++ ECS/ForgottenPassword.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ECS.BLL;
+using ECS.Model;
 
 namespace ECS
 {
@@ -16,7 +18,28 @@ namespace ECS
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+            if (ValidEntry())
+            {
+                string username = txtUserName.Text;
+                string last4ofPhone = txtPhone.Text;
 
+                Bll bll = new Bll();
+                string pin = bll.GetPIN(username, last4ofPhone);
+                string returnedValue = "Your PIN is: " + pin;
+                string radalertscript = "<script language='javascript'>function f(){radalert('" + returnedValue + "', 300, 100, 'ECS Volunteer App: Forgot Password'); Sys.Application.remove_load(f);}; Sys.Application.add_load(f);</script>";
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "radalert", radalertscript);
+
+            }
+
+        }
+
+        private bool ValidEntry()
+        {
+            bool valid = false;
+            if (!String.IsNullOrEmpty(txtPhone.Text) && !String.IsNullOrEmpty(txtUserName.Text))
+                valid = true;
+
+            return valid;
         }
 
         protected void btnCancel_Click(object sender, EventArgs e)
