324ed00cc17d45616d3d25172988feba606f13a9
n00846095 <cici.carter@gmail.com>
2017-02-11 12:10:56 -0500

    Added new stored proc for RunReports page
    Training session with Tillman. Available via WebEx.

---
+++ ECS/sql/procedures/SelectReportForMonth.sql
@@ -0,0 +1,17 @@
+/*
+	Created by Cici Carter (with Tillman Chen)
+	02-11-2017
+*/
+
+Create Proc SelectReportForMonth(@monthNum integer)
+as
+begin
+
+	select v.FirstName, v.LastName, c.CenterName, vl.DateTimeLogged, vl.NumberMinutes, t.TaskDescr, t.PricePerMinute,
+	vl.VolunteerID, vl.TaskID, vl.CenterID
+	from VolunteerLog vl
+	join Volunteer v on v.VolunteerID=vl.VolunteerID
+	join Task t on t.TaskID=vl.TaskID
+	join Center c on c.CenterID=vl.CenterID
+	where DATEPART(m, vl.DateTimeLogged) = @monthNum
+end
