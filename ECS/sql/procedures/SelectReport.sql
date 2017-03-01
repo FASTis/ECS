677c0f4916ebf95ba6f1494d6ebb116779ae2640
n00846095 <cici.carter@gmail.com>
2017-03-01 08:37:20 -0500

    Added Child to report proc

--- ECS/sql/procedures/SelectReport.sql
+++ ECS/sql/procedures/SelectReport.sql
@@ -7,12 +7,15 @@
 Create Proc SelectReport
 as
 begin
-	select v.LastName, v.FirstName, vt.VolunteerTypeDescr, c.CenterName, vl.DateTimeLogged, 
-	t.TaskDescr, vl.NumberMinutes, t.PricePerMinute, vl.NumberMinutes * t.PricePerMinute TotalValue,
-	v.VolunteerID, t.TaskID, c.CenterID, vt.VolunteerTypeID
+	select v.LastName + ', ' + v.FirstName Volunteer, vt.VolunteerTypeDescr, c.CenterName, vl.DateTimeLogged, 
+	t.TaskDescr, ch.LastName + ', ' + ch.FirstName Child, vl.NumberMinutes, t.PricePerMinute, vl.NumberMinutes * t.PricePerMinute TotalValue,
+	v.VolunteerID, t.TaskID, c.CenterID, vt.VolunteerTypeID,
+	ch.ChildId -- added for Parent volunteers
 	from VolunteerLog vl
 	join Volunteer v on v.VolunteerID=vl.VolunteerID
 	join Task t on t.TaskID=vl.TaskID
 	join Center c on c.CenterID=vl.CenterID
 	join VolunteerType vt on vt.VolunteerTypeID=v.VolunteerTypeID
+	left join Family f on f.VolunteerID=v.VolunteerID
+	left join Child ch on ch.ChildID=f.ChildID
 end
