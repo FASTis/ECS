d35b8bf6858487d203c5f915b3667491068f01cf
n00846095 <cici.carter@gmail.com>
2017-03-01 13:30:15 -0500

    Modified SelectReport proc

---
+++ ECS/SelectReport.sql
@@ -0,0 +1,34 @@
+/*
+	Created by Cici Carter
+	02-24-2017 
+	(to replace SelectReportByMonth; used by RunReports.aspx)
+
+	03-01-2017 - Ccarter - modified to add parameters
+*/
+
+Create Proc SelectReport
+(
+ @MonthNum int,
+ @TaskID int,
+ @VolunteerTypeID int,
+ @CenterID int
+ )
+ -- SelectReport 3, 0, 0, 0
+as
+begin
+	select v.LastName + ', ' + v.FirstName Volunteer, vt.VolunteerTypeDescr, c.CenterName, vl.DateTimeLogged, 
+	t.TaskDescr, ch.LastName + ', ' + ch.FirstName Child, vl.NumberMinutes, t.PricePerMinute, vl.NumberMinutes * t.PricePerMinute TotalValue,
+	v.VolunteerID, t.TaskID, c.CenterID, vt.VolunteerTypeID,
+	ch.ChildId -- added for Parent volunteers
+	from VolunteerLog vl
+	join Volunteer v on v.VolunteerID=vl.VolunteerID
+	join Task t on t.TaskID=vl.TaskID
+	join Center c on c.CenterID=vl.CenterID
+	join VolunteerType vt on vt.VolunteerTypeID=v.VolunteerTypeID
+	left join Child ch on ch.ChildID=vl.ChildID
+	where	(@MonthNum = 0 or (datepart(month, vl.DateTimeLogged)=@MonthNum))
+		and (@TaskID = 0 or t.TaskID=@TaskID)
+		and (@VolunteerTypeID = 0 or vt.VolunteerTypeID=@VolunteerTypeID)
+		and (@CenterID = 0 or c.CenterID=@CenterID)
+	order by vl.DateTimeLogged
+end
