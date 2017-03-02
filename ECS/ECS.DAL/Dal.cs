06006db00e65cd3074e112adeae1143d75c7da65
n00846095 <cici.carter@gmail.com>
2017-03-02 18:23:54 -0500

    Add Child functionality for InKind

--- ECS/ECS.DAL/Dal.cs
+++ ECS/ECS.DAL/Dal.cs
@@ -59,6 +59,7 @@ namespace ECS.DAL
             return retVal;
         }
 
+
         /// <summary>
         /// This updates info for an existing user.
         /// </summary>
@@ -441,7 +442,7 @@ namespace ECS.DAL
         /// <param name="numHrs"></param>
         /// <param name="numMins"></param>
         /// <returns></returns>
-        public string LogInKind(int volunteerId, int taskId, int centerId, int numHrs, int numMins)
+        public string LogInKind(int volunteerId, int childId, int taskId, int centerId, int numHrs, int numMins)
         {
             int totalMinutes = (60 * numHrs) + numMins;
             string retVal;
@@ -450,9 +451,9 @@ namespace ECS.DAL
                 using (SqlConnection conn = new SqlConnection(Conn))
                 {
                     //TODO: Convert this to a stored procedure, then use CommandType.StoredProcedure.
-                    string sql = String.Format(@"Insert {0} (VolunteerId, TaskId, CenterId, NumberMinutes)
-                                                 values ({1}, {2}, {3}, {4})", "VolunteerLog",
-                                                 volunteerId, taskId, centerId, totalMinutes);
+                    string sql = String.Format(@"Insert {0} (VolunteerId, ChildId, TaskId, CenterId, NumberMinutes)
+                                                 values ({1}, {2}, {3}, {4}, {5})", "VolunteerLog",
+                                                 volunteerId, childId, taskId, centerId, totalMinutes);
                     using (SqlCommand command1 = new SqlCommand(sql, conn))
                     {
                         command1.CommandType = CommandType.Text;
@@ -636,7 +637,29 @@ namespace ECS.DAL
             }
             return retVal;
         }
+        public DataTable GetChildren(int volunteerId)
+        {
+            DataTable dt = new DataTable();
 
+            using (SqlConnection conn = new SqlConnection(Conn))
+            {
+                //TODO: Convert this to a stored procedure, then use CommandType.StoredProcedure.
+                string sel = "select c.ChildID, c.LastName+', '+c.FirstName Child";
+                string tbl = "Child c join Family f on c.ChildID=f.FamilyID";
+                string cri = string.Format("f.VolunteerID='{0}'", volunteerId);
+                string sql = String.Format("{0} from {1} where {2}", sel, tbl, cri);
+
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.CommandType = CommandType.Text;
+                    conn.Open();
+                    dt.Load(command.ExecuteReader());
+                }
+            }
+            return dt;
+        }
+
+        //ad
         public bool ValidateUserInfo(string userId, string phone)
         {
             DataTable dtRetVal = new DataTable();
@@ -646,27 +669,24 @@ namespace ECS.DAL
                 using (SqlCommand command = new SqlCommand("ValidateUserInfo", conn))
                 {
                     command.CommandType = CommandType.StoredProcedure;
-
                     command.Parameters.AddWithValue("UserId", userId);
                     command.Parameters.AddWithValue("Last4DigitsOfPhone", phone);
-
                     conn.Open();
                     dtRetVal.Load(command.ExecuteReader());
-
-                    foreach (DataRow row in dtRetVal.Rows) // should only be one row.
+                    foreach (DataRow row in dtRetVal.Rows) // should only be one row.  
                     {
                         retVal = (int)row["ReturnValue"];
                     }
                     conn.Close();
                 }
-            }
-            if (retVal == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
+                if (retVal == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
         }
     }
