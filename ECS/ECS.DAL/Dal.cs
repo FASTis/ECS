9b1879d113c8f8d5df9e19f69c80f033122c977e
n00846095 <cici.carter@gmail.com>
2017-03-08 13:00:13 -0500

    Added Monthly Summary Report button

--- ECS/ECS.DAL/Dal.cs
+++ ECS/ECS.DAL/Dal.cs
@@ -689,5 +689,56 @@ namespace ECS.DAL
                 }
             }
         }
+
+        public object GetMonthlySummaryReport(int monthNumber)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(Conn))
+            {
+                //TODO: Convert this to a stored procedure, then use CommandType.StoredProcedure.
+                string sql = String.Format(@"declare @tbl table 
+                                            (Volunteer varchar(100), 
+                                             VolunteerTypeDescr varchar(100), 
+                                             CenterName varchar(100), 
+                                             DateTimeLogged datetime,  
+                                             TaskDescr varchar(100), 
+                                             Child varchar(100), 
+                                             NumberMinutes integer, 
+                                             PricePerMinute decimal, 
+                                             TotalValue money, 
+                                             VolunteerID integer, 
+                                             TaskID integer, 
+                                             CenterID integer, 
+                                             VolunteerTypeID integer, 
+                                             ChildId integer)
+
+                                            insert @tbl
+                                            exec SelectReport {0},0,0,0
+
+                                            SELECT 	CenterName as Center,
+	                                            sum(TotalValue) as [Volunteers(6720)],
+	                                            0 as [Goods & Services(6740)],
+	                                            0 as [Donated Space(6740)],
+	                                            0 as [Donated Equipment(6740)],
+	                                            0 as [Prof. Svs(6740)],
+	                                            0 as [Travel(6730)],
+	                                            sum(TotalValue) as [Center Total],
+	                                            0 as [Rent(6710)],
+	                                            0 as [Small Grants],
+	                                            0 as [Canteen(6740)],
+	                                            sum(TotalValue) as [TOTAL]
+                                            from @tbl
+                                            Group By CenterName", monthNumber);
+
+                using (SqlCommand command = new SqlCommand(sql, conn))
+                {
+                    command.CommandType = CommandType.Text;
+                    conn.Open();
+                    dt.Load(command.ExecuteReader());
+                }
+            }
+            return dt;
+        }
     }
 }
