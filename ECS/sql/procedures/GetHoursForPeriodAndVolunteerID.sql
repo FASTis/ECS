50f7a0928f33e3d8552192d9bd8757c147ee161e
n00846095 <cici.carter@gmail.com>
2017-04-13 00:58:03 -0400

    Updated

--- ECS/sql/procedures/GetHoursForPeriodAndVolunteerID.sql
+++ ECS/sql/procedures/GetHoursForPeriodAndVolunteerID.sql
@@ -2,7 +2,7 @@
 Create Procedure GetHoursForPeriodAndVolunteerID (@startDate datetime, @endDate datetime, @VolunteerId int)
 As
 /*
-GetHoursForPeriodAndVolunteerID '3/1/2017','4/1/2017',4
+GetHoursForPeriodAndVolunteerID '4/1/2017','4/30/2017',4
 
 declare @startDate datetime, @endDate datetime, @VolunteerId int
 set @startDate='3/1/2017'
@@ -10,11 +10,21 @@ set @endDate='4/1/2017'
 set @VolunteerId=4
 */
 BEGIN
-	select isnull(convert(decimal(18,2), sum(vl.NumberMinutes)/60.00),0.00) HoursForPeriod
+	select x.HoursForPeriod, y.HoursForPeriod as NonHomeHoursPerPeriod from
+	(select @volunteerID ID, isnull(convert(decimal(18,2), sum(vl.NumberMinutes)/60.00),0.00) HoursForPeriod
+	from VolunteerLog vl
+	join Volunteer v on v.VolunteerID=vl.VolunteerID
+	join VolunteerType vt on v.VolunteerTypeID=vt.VolunteerTypeID
+	where vl.DateTimeLogged between @startDate and @endDate
+	and v.VolunteerId=@volunteerId) as x
+	join
+	(select @volunteerID ID, isnull(convert(decimal(18,2), sum(vl.NumberMinutes)/60.00),0.00) HoursForPeriod
 	from VolunteerLog vl
 	join Volunteer v on v.VolunteerID=vl.VolunteerID
 	join VolunteerType vt on v.VolunteerTypeID=vt.VolunteerTypeID
 	where vl.TaskId not in (1,5)
 	and vl.DateTimeLogged between @startDate and @endDate
-	and v.VolunteerId=@volunteerId
+	and v.VolunteerId=@volunteerId) as y
+	on x.ID=y.ID
+
 END
