f233ed010fc1054841cd2d290fd62e83aed46324
n00846095 <cici.carter@gmail.com>
2017-04-16 07:05:42 -0400

    Added AddChild and fixed GetChildrenByVolunteerID bug

---
+++ ECS/sql/procedures/AddChild.sql
@@ -0,0 +1,33 @@
+create procedure AddChild ( 
+	  @volunteerID int
+	, @childFName varchar(30)
+	, @childLName varchar(30)
+	, @dob datetime
+	, @relationship varchar(20) -- Volunteer's relationship to child.
+	)
+as
+-- AddChild 2, 'Lisa','Carter','3/29/2014','Mother'
+-- GetChildrenByVolunteerID 2
+begin
+
+	declare @familyID int
+	declare @childID int
+
+	insert Child (FirstName, LastName, DOB)
+	values (@childFName, @childLName, @dob)
+
+	set @childID=SCOPE_IDENTITY() -- to get the ID of the row just created.
+
+	-- does this volunteer currently exist in the family table?
+	select  @familyID=FamilyID from Family where VolunteerID=@volunteerID
+
+	-- if volunteer does not exist, create a unique id for this volunteer.
+	if (isnull(@familyID,0) = 0)
+		select @familyID=isnull(max(FamilyID),0)+1 from Family
+	
+	-- insert record into Family table to join child to volunteer
+	SET IDENTITY_INSERT Family ON
+	insert Family (FamilyID, ChildID, VolunteerID, Descr)
+	values (@familyID, @childID, @volunteerID, @relationship)
+	SET IDENTITY_INSERT Family OFF
+end
--- ECS/sql/procedures/GetChildrenByVolunteerID.sql
+++ ECS/sql/procedures/GetChildrenByVolunteerID.sql
@@ -2,7 +2,7 @@ Create Procedure GetChildrenByVolunteerID
 (@volunteerId int)
 as
 begin
-	select c.ChildID, c.LastName+', '+c.FirstName Child
-    from Child c join Family f on c.ChildID=f.FamilyID
+	select c.ChildID, c.LastName+', '+c.FirstName Child, c.DOB
+    from Child c join Family f on c.ChildID=f.ChildID
     where f.VolunteerID=@volunteerId
 end
