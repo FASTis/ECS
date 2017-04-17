6f65d49f0a267d1df17a21f615e2bfbf67e21daa
n00846095 <cici.carter@gmail.com>
2017-04-16 22:11:18 -0400

    Updated sprocs for Add Child functions.

--- ECS/sql/procedures/GetChildrenByVolunteerID.sql
+++ ECS/sql/procedures/GetChildrenByVolunteerID.sql
@@ -2,7 +2,7 @@ Create Procedure GetChildrenByVolunteerID
 (@volunteerId int)
 as
 begin
-	select c.ChildID, c.LastName+', '+c.FirstName Child, c.DOB, f.Descr Relationship
+	select c.ChildID, c.LastName+', '+c.FirstName Child, c.DOB, f.Descr Relationship, @volunteerId VolunteerID
     from Child c join Family f on c.ChildID=f.ChildID
     where f.VolunteerID=@volunteerId
 end
