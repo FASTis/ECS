ed1cab2ebbe2ec95c803cc50c052ff445ba93619
n00846095 <cici.carter@gmail.com>
2017-04-16 22:12:23 -0400

    Added Child class.

---
+++ ECS/ECS.Model/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECS.Model
+{
+    public class Child
+    {
+        public int ChildID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Relationship { get; set; }
+
+    }
+}
