74c20a787c782b46e2728e6b038f5699cadd4cb9
n00846095 <cici.carter@gmail.com>
2017-04-21 00:13:03 -0400

    Added "Log Off" button on bar, and other changes
    Removed "Modified Date" verbiage and replaced with Log Off button.
    Made Log Off button invisible unless user is actually logged on.
    Changed breadcrumbs to go back to Profile from Edit Child and Manage Children.

---
+++ Site.Master.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.SessionState;
+
+namespace ECS
+{
+    public partial class ECSSite : System.Web.UI.MasterPage
+    {
+        public string sessionUser;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetSessionUser();
+        }
+
+        public void SetSessionUser()
+        {
+            try
+            {
+                sessionUser = String.Format(" ({0} {1})", Session["FirstName"].ToString(), Session["LastName"].ToString());
+                divUser.InnerText = sessionUser;
+                btnLogoff.Visible = true;
+            }
+            catch (NullReferenceException)
+            {
+                sessionUser = "";
+                btnLogoff.Visible = false;
+            }
+        }
+        protected void btnLogoff_Click(object sender, EventArgs e)
+        {
+            Default main = new Default();
+            main.DoLogoff();
+        }
+
+        
+    }
+}
