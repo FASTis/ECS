a0e2c8de4225691249079414eb5aa6ba52f39a57
n00846095 <cici.carter@gmail.com>
2017-05-03 13:12:27 -0400

    Rollback previous change.

--- ECS/sql/procedures/GetChildrenByVolunteerID.sql
+++ ECS/sql/procedures/GetChildrenByVolunteerID.sql
@@ -5,6 +5,4 @@ begin
 	select c.ChildID, c.LastName+', '+c.FirstName Child, c.DOB, f.Descr Relationship, @volunteerId VolunteerID
     from Child c join Family f on c.ChildID=f.ChildID
     where f.VolunteerID=@volunteerId
-	union select 0,'N/A',null,null,@volunteerId
-
 end
