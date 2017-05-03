b000c4a58cf4916d10f8767781b72c2d55a7e1b7
n00846095 <cici.carter@gmail.com>
2017-05-03 09:16:12 -0400

    Set the Active flag on all tables

---
+++ ECS/sql/scripts/Script_SetActiveFlagOnAllTables.sql
@@ -0,0 +1,11 @@
+-- set new Active flag on all tables.
+update Center set Active=1
+update Child set Active=1
+update Company set Active=1
+update Family set Active=1
+update Staff set Active=1
+update StaffType set Active=1
+update Task set Active=1
+update UserLogon set Active=1
+update Volunteer set Active=1
+update VolunteerType set Active=1
