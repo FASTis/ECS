edfcab19e65f4a7d7c2f4879b6f1ef5fc87d8dca
n00846095 <cici.carter@gmail.com>
2017-05-03 14:51:41 -0400

    Implemented new Add/Edit Task functionality

--- ECS/EditTask.aspx
+++ ECS/EditTask.aspx
@@ -6,4 +6,42 @@
         <li><a href="ManageTasks.aspx">Manage Tasks</a></li>
         <li class="active">Edit Task</li>
     </ul>
+
+    <div class="form-horizontal">
+        <fieldset>
+            <legend>Add/Edit Task</legend>
+
+            <!-- form entry -->
+            <div class="form-group">
+                <asp:Label ID="lblTaskDescr" runat="server" Text="Task Description:" CssClass="col-lg-2 control-label"></asp:Label>
+                <div class="col-lg-10">
+                    <asp:TextBox ID="txtTaskDescription" runat="server" placeholder="" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lblPricePerMinute" runat="server" Text="Price Per Minute:" CssClass="col-lg-2 control-label"></asp:Label>
+                <div class="col-lg-10">
+                    <asp:TextBox ID="txtPricePerMinute" runat="server" placeholder="" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <asp:Label ID="lblRestrictToTypes" runat="server" Text="RestrictToTypes (ex: 1,2):" CssClass="col-lg-2 control-label"></asp:Label>
+                <div class="col-lg-10">
+                    <asp:TextBox ID="txtRestrictToTypes" runat="server" placeholder="" CssClass="form-control"></asp:TextBox>
+                </div>
+            </div>
+
+            <!-- submit/cancel button -->
+            <br />
+            <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-2">
+                    <asp:Button ID="btnSubmit" runat="server" CssClass="btn btn-primary" Text="Submit" OnClick="btnSubmit_Click" />
+                    <asp:Button ID="btnCancel" runat="server" CssClass="btn btn-warning" Text="Cancel" OnClick="btnCancel_Click" />
+                </div>
+            </div>
+        </fieldset>
+    </div>
+
 </asp:Content>
--- ECS/EditTask.aspx.cs
+++ ECS/EditTask.aspx.cs
@@ -4,14 +4,87 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ECS.BLL;
+using ECS.Model;
 
 namespace ECS
 {
     public partial class EditTask : System.Web.UI.Page
     {
+        //Instantiate a new BLL object so that you can access its methods later...
+        Bll _bll = new Bll();
+        string _mode = "";
+        string _id = "";
+        string _volId = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Get the values in the query string and store them in variables...
+            _mode = Request.QueryString["mode"];
 
+            if (_mode != "A") //if mode is not "A" for "add", then get the id from the query string and populate the staff type textbox.
+            {
+                if (!IsPostBack) //Don't do this on postback -- don't do the below unless we're loading the form for the first time!!!
+                {
+                    _id = Request.QueryString["id"];
+                    Task t = _bll.GetTaskById(Convert.ToInt32(_id));
+                    txtTaskDescription.Text = t.TaskDescr;
+                    txtPricePerMinute.Text = t.PricePerMinute.ToString();
+                    txtRestrictToTypes.Text = t.RestrictToVolunteerTypes;
+                }
+            }
+
+            if (_mode == "D")
+                btnSubmit.Text = "Delete";
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            _id = Request.QueryString["id"];
+            _volId = Request.QueryString["volId"];
+
+            Task task = new Task();
+            task.TaskID = Convert.ToInt32(_id);
+            task.TaskDescr = txtTaskDescription.Text;
+            task.PricePerMinute = Convert.ToDecimal(txtPricePerMinute.Text);
+            task.RestrictToVolunteerTypes = txtRestrictToTypes.Text;
+
+            string returnedMessage = "";
+
+            switch (_mode)
+            {
+                case "E":
+                    returnedMessage = _bll.EditTask(task);
+                    break;
+                case "A":
+                    returnedMessage = _bll.AddTask(task);
+                    break;
+                case "D":
+                    returnedMessage = String.Format("Please ask your system administrator to deactivate Task ID {0} - {1}.", _id, task.TaskDescr);
+                    btnCancel.Text = "Close Form";
+                    break;
+                default:
+                    break;
+            }
+            //Return the value to the user.
+            DisplayPopup(returnedMessage);
+
+            if (returnedMessage.Contains("Success"))
+            {
+                btnCancel.Text = "Close Form";
+            }
+
+
+        }
+
+        private void DisplayPopup(string returnedMessage)
+        {
+            string radalertscript = "<script language='javascript'>function f(){radalert('" + returnedMessage.Replace("'", "`") + "', 300, 100, 'ECS Volunteer App: Edit Child'); Sys.Application.remove_load(f);}; Sys.Application.add_load(f);</script>";
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "radalert", radalertscript);
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ManageTasks.aspx");
         }
     }
 }
