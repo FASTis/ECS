536271d70b6b2626277752c914f303df51c86b25
n00846095 <cici.carter@gmail.com>
2017-05-03 14:48:04 -0400

    Modifications to existing sprocs.

--- ECS/sql/procedures/SelectTask.sql
+++ ECS/sql/procedures/SelectTask.sql
@@ -1,7 +1,7 @@
 ﻿Create Procedure SelectTask
 as
 begin
-	Select TaskID, TaskDescr, PricePerMinute, PricePerMinute * 60 as PricePerHour from Task
+	Select TaskID, TaskDescr, PricePerMinute, PricePerMinute * 60 as PricePerHour, RestrictToVolunteerTypes from Task
 	Order By TaskDescr
 end
 go
