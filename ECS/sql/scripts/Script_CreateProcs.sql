b14f6035a4187ae5a0f6ab09084cb0ed9cda8729
n00846095 <cici.carter@gmail.com>
2017-05-04 19:53:34 -0400

    Fixed Bug in AddCenter sproc
    Edit Center - Address field is truncated on save. (Fixed with this check-in.)

--- ECS/sql/scripts/Script_CreateProcs.sql
+++ ECS/sql/scripts/Script_CreateProcs.sql
@@ -1,9 +1,9 @@
 Create Procedure AddCenter
-(
-	 @centerName varchar(25)
-	,@streetAddr varchar(25)
-	,@city varchar(50) -- increase length.
-	,@zip varchar(25)
+(	-- Make sure lengths match the table!
+	 @centerName varchar(50)
+	,@streetAddr varchar(50)
+	,@city varchar(30)
+	,@zip varchar(5)
 )
 
 as
