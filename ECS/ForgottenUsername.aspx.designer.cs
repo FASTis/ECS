b5698f3194f2c59f3419d79a46bb4d769d551d3a
jabeau <ashley.j.darling@outlook.com>
2017-06-11 19:55:28 -0400

    checking in missing file

--- ECS/ForgottenUsername.aspx.designer.cs
+++ ECS/ForgottenUsername.aspx.designer.cs
@@ -83,5 +83,14 @@ namespace ECS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCancel;
+
+        /// <summary>
+        /// lblRetVal control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRetVal;
     }
 }
